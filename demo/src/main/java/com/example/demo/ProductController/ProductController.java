abe18d3f8b60b22dd3c0e3416cbf3c98aed3f27c
ltsazp4 <Guillaume.Rostagnat@liebherr.com>
2025-04-15 09:55:25 +0200

    Product entity

--- demo/src/main/java/com/example/demo/ProductController/ProductController.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo.ProductController;
-
-public class ProductController {
-
-}
--- demo/src/main/java/com/example/demo/models/Product.java
+++ demo/src/main/java/com/example/demo/models/Product.java
@@ -1,5 +1,88 @@
 package com.example.demo.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+
+@Entity
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    // Attributs
+    private Long id;
+    private String name;
+    private String description;
+    private double price;
+
+    // constructeurs
+    public Product() {
+
+    }
+
+    public Product(String name, String description, double price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+
+    }
+
+    // Getters et setters
+
+    /**
+     * @return Long return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return String return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return String return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * @return double return the price
+     */
+    public double getPrice() {
+        return price;
+    }
+
+    /**
+     * @param price the price to set
+     */
+    public void setPrice(double price) {
+        this.price = price;
+    }
 
 }
