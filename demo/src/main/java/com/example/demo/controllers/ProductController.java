c661467de9e769e79e35751d4f63362e72e50ba3
ltsazp4 <Guillaume.Rostagnat@liebherr.com>
2025-04-15 16:38:47 +0200

    Product Controller

--- demo/src/main/java/com/example/demo/controllers/ProductController.java
+++ demo/src/main/java/com/example/demo/controllers/ProductController.java
@@ -1,5 +1,60 @@
 package com.example.demo.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.models.Product;
+import com.example.demo.repository.ProductRepository;
+
+@RestController
+@RequestMapping("/api/products")
+
 public class ProductController {
 
+    @Autowired
+    private ProductRepository productRepository;
+
+    // GET all products
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+
+    }
+
+    // GET products by Id
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable long id) {
+        return productRepository.findById(id).orElse(null);
+
+    }
+
+    // POST Create product
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        return productRepository.save(product);
+
+    }
+
+    // PUT Update product
+    @PutMapping("/id")
+    public Product updateProduct(@PathVariable long id, @RequestBody Product product) {
+        // Recherche du produit désiré
+        Product produitExistant = productRepository
+                .findById(id).orElseThrow(() -> new RuntimeException("Ce produit n'existe pas"));
+        // Mise à des jours des attribites du produit
+        produitExistant.setName(product.getName());
+        produitExistant.setDescription((product.getDescription()));
+        produitExistant.setPrice(product.getPrice());
+
+        return productRepository.save(produitExistant);
+
+    }
 }
--- demo/src/main/java/com/example/demo/repository/ProductRepository.java
+++ demo/src/main/java/com/example/demo/repository/ProductRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.models.Product;
 
-public interface Repository extends JpaRepository<Product, Long>{
+public interface ProductRepository extends JpaRepository<Product, Long>{
 
 }
