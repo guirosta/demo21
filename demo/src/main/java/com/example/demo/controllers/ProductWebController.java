c8811e14b14d0dac21dbb8316f49dd7025f9a5ea
ltsazp4 <Guillaume.Rostagnat@liebherr.com>
2025-04-26 18:09:39 +0200

    product-form.html

--- demo/src/main/java/com/example/demo/controllers/ProductWebController.java
+++ demo/src/main/java/com/example/demo/controllers/ProductWebController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 @RequestMapping("/products") // Nouvelle url pour le webcontroller
@@ -18,6 +21,7 @@ public class ProductWebController {
     @Autowired
     private ProductRepository productRepository;
 
+    // Pahe principale
     @GetMapping
     public String showProducts(Model model) {
         List<Product> products = productRepository.findAll();
@@ -25,12 +29,30 @@ public class ProductWebController {
         return "products"; // correspond à templates/products.html
     }
 
+    // Formulaire d'ajout
+    @GetMapping("/new")
+    public String showCreateForm(Model model) {
+        model.addAttribute("product", new Product());
+        return "product-form"; // correspond à templates/product-form.html
+
+    }
+
+    // Page de produit
     @GetMapping("/{id}")
     public String showProductDetails(@PathVariable long id, Model model) {
         Product product = productRepository.findById(id)
-        .orElseThrow(() -> new IllegalArgumentException("Produit introuvable")) ;
+                .orElseThrow(() -> new IllegalArgumentException("Produit introuvable"));
         model.addAttribute("product", product);
         return "product-details"; // correspond à templates/product-details.html
 
     }
+
+    // Création d'un produit
+    @PostMapping
+    public String createProduct(Product product) {
+        productRepository.save(product);
+        return "redirect:/products";
+
+    }
+
 }
